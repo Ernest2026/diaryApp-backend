88a1ac68ea87b5df35e87fb29653414356e561bc
Adophilus <uchenna19of@gmail.com>
2023-10-28 11:31:33 +0100

    removed merge conflict marker

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,12 @@
 import "express-async-errors"
 
 import express from 'express'
-<<<<<<< HEAD:src/index.ts
 import config from '@/config'
 import logger from '@/utils/logger'
 import ErrorHandlerMiddleware from "./middleware/error-handler.middleware"
 import LoggerMiddleware from "./middleware/logger.middleware"
 import { prisma } from './utils/database'
 import AppRouter from './modules'
-=======
-import config from './config/index'
-import logger from './utils/logger'
-// import ErrorHandlerMiddleware from "./middleware/error-handler.middleware"
-// import LoggerMiddleware from "./middleware/logger.middleware"
-import { prisma } from './services/database.service'
-import AppRouter from './routers'
->>>>>>> main:src/app.ts
 import morgan from 'morgan'
 import cors from 'cors'
 import { ReasonPhrases, StatusCodes } from "http-status-codes"
@@ -26,29 +17,27 @@ process.on("beforeExit", async () => {
   await prisma.$disconnect()
 })
 
-// export function bootstrap() {
+export function bootstrap() {
   app.use(express.urlencoded({ extended: false }))
   app.use(express.json())
 
   app.options("*", cors())
   app.use(morgan('combined'))
 
-  // app.use(LoggerMiddleware)
-
-  app.get("/", (_, res) => res.send("Do you like indomie?"));
+  app.use(LoggerMiddleware)
 
   app.use('/api/v1/', AppRouter)
 
   app.use((req, res) => {
     res.status(StatusCodes.NOT_FOUND).json({ error: ReasonPhrases.NOT_FOUND })
   })
-  // app.use(ErrorHandlerMiddleware)
-// }
+  app.use(ErrorHandlerMiddleware)
+}
 
-// export function run() {
-  // app.listen(config.server.port, () => {
-  //   logger.info(`Server running on port ${config.server.port}`)
-  // })
-// }
+export function run() {
+  app.listen(config.server.port, () => {
+    logger.info(`Server running on port ${config.server.port}`)
+  })
+}
 
 export default app
